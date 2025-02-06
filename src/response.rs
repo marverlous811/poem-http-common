d98cae79f5e4acdd28bbbfa3c9e6a1483d56c6e6
Shadow walker <hieu811@gmail.com>
2025-02-06 18:00:16 +0700

    feat: add some detail error for response

--- src/response.rs
+++ src/response.rs
@@ -1,9 +1,33 @@
+use poem::{http::StatusCode, Error};
 use poem_openapi::{
     payload::Json,
     types::{ParseFromJSON, ToJSON},
     ApiResponse, Object,
 };
 use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Object, Clone)]
+pub struct StatusResponse {
+    status: bool,
+}
+
+pub fn build_error_response(code: StatusCode, msg: &str) -> Error {
+    let err: HttpApiResponse<StatusResponse> = match code {
+        StatusCode::BAD_REQUEST => HttpApiResponse::BadRequest(ErrorResponse::from_str(msg).to_json()),
+        StatusCode::UNAUTHORIZED => HttpApiResponse::Unauthorized(ErrorResponse::from_str(msg).to_json()),
+        StatusCode::FORBIDDEN => HttpApiResponse::Forbidden(ErrorResponse::from_str(msg).to_json()),
+        StatusCode::TOO_MANY_REQUESTS => HttpApiResponse::TooManyRequest(ErrorResponse::from_str(msg).to_json()),
+        StatusCode::NOT_FOUND => HttpApiResponse::NotFound(ErrorResponse::from_str(msg).to_json()),
+        StatusCode::INTERNAL_SERVER_ERROR => {
+            HttpApiResponse::InternalServerError(ErrorResponse::from_str(msg).to_json())
+        }
+        StatusCode::BAD_GATEWAY => HttpApiResponse::BadGateway(ErrorResponse::from_str(msg).to_json()),
+        StatusCode::SERVICE_UNAVAILABLE => HttpApiResponse::ServiceUnavailable(ErrorResponse::from_str(msg).to_json()),
+        _ => HttpApiResponse::InternalServerError(ErrorResponse::from_str(msg).to_json()),
+    };
+
+    Error::from(err)
+}
 
 #[derive(Serialize, Deserialize, Debug, Object, Clone)]
 pub struct ErrorResponse {
@@ -35,6 +59,12 @@ where
     Forbidden(Json<ErrorResponse>),
     #[oai(status = 404)]
     NotFound(Json<ErrorResponse>),
+    #[oai(status = 429)]
+    TooManyRequest(Json<ErrorResponse>),
     #[oai(status = 500)]
     InternalServerError(Json<ErrorResponse>),
+    #[oai(status = 502)]
+    BadGateway(Json<ErrorResponse>),
+    #[oai(status = 503)]
+    ServiceUnavailable(Json<ErrorResponse>),
 }
